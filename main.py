01d51e7ef3a84335fafbf1db8a08fe99622253c7
reemallicious <reemsoud1993@gmail.com>
2023-02-06 15:30:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def main():
     name = input("Enter your name: ")
-    print(f"Your name is {name}.")
+    age = int(input('How old are you: '))
+    print(f"Your name is {name} and you are {age} years old.")
     
     
 if __name__ == "__main__":
